ff644920abe25473325b45f20bc7ec676b4e54fe
HugoHead <45836267+HugoHead@users.noreply.github.com>
2020-02-15 14:59:34 -0500

    Add files via upload

---
+++ memorizelist.rb
@@ -0,0 +1,10 @@
+puts "memeorize lists!!"
+
+list = ['alif', "baa", "taa", "thaa", "jeem", "Haa", "KHaa", "daal", "dhaal", "raa", "zaay", "seen", "sheen", "sOd", "dOd", "TTaa'", "THaa'", "Ayn", "Ghayn", "faa", "Qaaf", "Kaaf", "lam", "meme", "noon", "haa", "wow", "ya"]
+
+puts list[0]
+
+list.each do |n|
+	print n
+	gets
+end
